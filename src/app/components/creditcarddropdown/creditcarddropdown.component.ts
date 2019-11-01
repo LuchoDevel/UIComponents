5da68f1c3639921677b6bf050012e8ead79292a4
Luchodevel <luis.morales.ud@gmail.com>
2019-11-01 01:00:20 -0500

    Ajuste para dropdown de tarjetas

--- src/app/components/creditcarddropdown/creditcarddropdown.component.ts
+++ src/app/components/creditcarddropdown/creditcarddropdown.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+export interface ListCreditCard {
+  imagen: string;
+  titulo?: string;
+  descripcion: string;
+  valor?: any;
+}
 
 @Component({
   selector: 'app-creditcarddropdown',
@@ -7,9 +15,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreditcarddropdownComponent implements OnInit {
 
-  constructor() { }
+  private select: any;
+  private opciones: any;
+  private contenidoSelect: any;
 
-  ngOnInit() {
+  public listCard:ListCreditCard[];
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+
+    this.listCard = [
+      {descripcion: '**** 4512', imagen: 'assets/img/ico/card-master-ico.png', valor: '**** 4512'},
+      {descripcion: '**** 666', imagen: 'assets/img/ico/card-visa-ico.png', valor: '**** 666'}
+
+    ]
   }
 
+  ngOnInit() {
+    this.initVar();
+  }
+
+  initVar() {
+    this.select = this.document.querySelector('#select');
+    this.opciones = this.document.querySelector('#opciones');
+    this.contenidoSelect  = this.document.querySelector('#select .contenido-select');
+  }
+
+  activeSelect(){
+    this.select.classList.toggle('active');
+    this.opciones.classList.toggle('active');
+  }
+
+  selectOption(card, $e){
+    $e.preventDefault();
+    this.contenidoSelect.innerHTML = $e.currentTarget.innerHTML;
+    // console.log($e.currentTarget.innerHTML);
+    console.log(card);
+  }
 }
